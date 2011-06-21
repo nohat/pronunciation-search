3711c87875d80ae3e2085378771ec7a014cce111
David Friedland <friedland@groupon.com>
2011-06-21 07:40:18 -0500

    more spec tests for query parsing

--- spec/models/pronunciation_spec.rb
+++ spec/models/pronunciation_spec.rb
@@ -29,5 +29,25 @@ describe Pronunciation do
     it "converts multiple vowels with no stress to a wildcard stress search" do
       Pronunciation.parse_query("/AH B AA B/").should == ["arpabet REGEXP ?", ["AH. B AA. B"]]
     end
+
+    it "handles both pronunciation and spelling in the same query" do
+      Pronunciation.parse_query("/F UW1/ <foo>").should == ["words.name REGEXP ? AND arpabet REGEXP ?", ["foo", "F UW1"]]
+    end
+
+    it "handles multiple pronunciations" do
+      Pronunciation.parse_query("/ZH/ /JH/").should == ["arpabet REGEXP ? AND arpabet REGEXP ?", ["ZH", "JH"]]
+    end
+
+    it "handles multiple spellings" do
+      Pronunciation.parse_query("<foo> <bar>").should == ["words.name REGEXP ? AND words.name REGEXP ?", ["foo", "bar"]]
+    end
+
+    it "raises if spelling contains invalid characters" do
+      lambda { Pronunciation.parse_query("<f|>") }.should raise_error "Spelling contains invalid characters: |"
+    end
+
+    it "raises if pronunciation contains invalid characters" do
+      lambda { Pronunciation.parse_query("/<F UW!1/") }.should raise_error "Pronunciation contains invalid characters: <!"
+    end
   end
 end
